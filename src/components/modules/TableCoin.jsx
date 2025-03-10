fc06a28185263c9bc3f61e35565d7c9db96e8c63
Mehdi Riazi <mohammadmehdi.riazi@gmail.com>
2025-03-10 15:36:01 +0330

    in Process

---
+++ src/components/modules/TableCoin.jsx
@@ -0,0 +1,36 @@
+
+function TableCoin({coins}) {
+    console.log(coins); 
+  return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Coin</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>24h</th>
+                    <th>Total Volume</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {coins.map(coin => <tr key={coin.id}>
+                    <td>
+                        <div>
+                            <img src={coin.image} alt=""/>
+                            <span>{coin.symbol.toUpperCase()}</span>
+                        </div>
+                    </td>
+                    <td>{coin.name}</td>
+                    <td>${coin.current_price.toLocaleString()}</td>
+                    <td>{coin.price_change_percentage_}%</td>
+                    <td>{coin.name}</td>
+                </tr>)}
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TableCoin
