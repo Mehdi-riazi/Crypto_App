d916d1598f2a6dbdcaa0b843ad3b1993d9e3d886
Mehdi Riazi <mohammadmehdi.riazi@gmail.com>
2025-03-13 02:06:00 +0330

    icons

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from 'react'
-
 import TableCoin from '../modules/TableCoin';
 import { getCoinList } from '../../services/cryptoApi';
 
 function HomePage() {
-  const [coins , setCoins] = useState ([])
-
+  const [coins , setCoins] = useState ([]);
+  const [isLoading , setIsLoading] = useState(true);
   useEffect (() => {
-
     const getData = async () => {
       const res = await fetch (getCoinList())
       const json = await res.json();
       setCoins(json);
+      setIsLoading(false);
     };
     getData();
   } , [])
 
   return (
     <div>
-      <TableCoin coins={coins}/>
+      <TableCoin coins={coins} isLoading={isLoading}/>
     </div>
   )
 }
